1630ddb4464ad6ceb7e3d87480c8678678c6d3d3
omidnw <omidnw@gmail.com>
2021-06-22 18:19:44 +0300

    add create database add create table add deletetable add insert value.

--- library/sqlite3mgt/sqlite3mgt.c
+++ library/sqlite3mgt/sqlite3mgt.c
@@ -4,7 +4,9 @@ sqlite3 *db;
 char sqlarray[255], *err_msg = 0;
 int rc;
 
-void open_sqlite_database_with_path(gchar *gdatabasename){
+void open_sqlite_database_with_path(gdatabasename)
+gchar *gdatabasename;
+{
     if (databasename == NULL) {
         databasename = malloc(512 * sizeof(gchar));
     }
@@ -19,7 +21,9 @@ void open_sqlite_database_with_path(gchar *gdatabasename){
     }
 }
 
-void sqlite_create_table(gchar *tablename, gchar *columnnames){
+void sqlite_create_table(tablename, columnnames)
+gchar *tablename, *columnnames;
+{
     if (databasename == NULL){
         g_warning("database name empty please try again!");
     }
@@ -43,6 +47,55 @@ void sqlite_create_table(gchar *tablename, gchar *columnnames){
     }
 }
 
-void sqlite_delete_table(){
+void sqlite_delete_table(tablename)
+gchar *tablename;
+{
+    if (databasename == NULL){
+        g_warning("database name empty please try again!");
+    }
+    else{
+        sqlite3_open(databasename, &db);
     
+        snprintf(sqlarray, sizeof(sqlarray), "DROP TABLE IF EXISTS %s;", tablename);
+        g_print("database address: %s | sql query: %s", databasename, sqlarray);
+        rc = sqlite3_exec(db, sqlarray, 0, 0, &err_msg);
+        if(rc != SQLITE_OK)
+        {
+            g_warning("%s", err_msg);
+            sqlite3_free(err_msg);
+            sqlite3_close(db);
+        }
+        else
+        {
+            g_print("table successfuly created.");
+            sqlite3_close(db);
+        }
+    }
+}
+
+void sqlite_insert_value_to_table(tablename, columnnames, value)
+gchar *tablename, *columnnames, *value;
+{
+    if (databasename == NULL){
+        g_warning("database name empty please try again!");
+    }
+    else{
+        sqlite3_open(databasename, &db);
+    
+        snprintf(sqlarray, sizeof(sqlarray), "INSERT INTO %s(%s) VALUES(%s);", tablename,
+                     columnnames, value);
+        g_print("database address: %s | sql query: %s", databasename, sqlarray);
+        rc = sqlite3_exec(db, sqlarray, 0, 0, &err_msg);
+        if(rc != SQLITE_OK)
+        {
+            g_warning("%s", err_msg);
+            sqlite3_free(err_msg);
+            sqlite3_close(db);
+        }
+        else
+        {
+            g_print("insert value to table successfuly.");
+            sqlite3_close(db);
+        }
+    }
 }
--- library/sqlite3mgt/sqlite3mgt.h
+++ library/sqlite3mgt/sqlite3mgt.h
@@ -5,5 +5,9 @@
 #include "../sqlite3/sqlite3.h"
 
 gchar *databasename;
+void open_sqlite_database_with_path(gchar *gdatabasename);
+void sqlite_create_table(gchar *tablename, gchar *columnnames);
+void sqlite_delete_table(gchar *tablename);
+void sqlite_insert_value_to_table(gchar *tablename, gchar *columnnames, gchar *value);
 
 #endif
