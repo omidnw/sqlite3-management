8b6e31e2b05485ecd228e738bb1323afb3ef8b0e
omid reza keshtkar <omidnw@outlook.com>
2021-07-23 21:58:01 +0430

    update main file

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <gtk/gtk.h>
+#include "library/gui/pages/home/home.h"
 
-int main(){
+int main(int argc, char *argv[]) {
+
+    gtk_init(&argc, &argv);
+
+    create_home_page();
+
+    gtk_main();
+
     return 0;
 }
