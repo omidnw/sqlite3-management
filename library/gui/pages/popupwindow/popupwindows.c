116a138355fee7c19a82ad4c75047fcbfe488a18
omid reza keshtkar <omidnw@outlook.com>
2021-08-09 22:21:57 +0430

    update popupwindow

--- library/gui/pages/popupwindow/popupwindows.c
+++ library/gui/pages/popupwindow/popupwindows.c
@@ -1,6 +1,9 @@
 #include "popupwindows.h"
 
 gboolean sqlitemgt_is_database_open();
+void checknullerror(GtkWidget *g_widget);
+void sqlitemgt_open_database_with_path(gchar *gdatabasename);
+
 
 void on_menu_open(GtkWindow *menuitem,
                   gpointer user_data)
@@ -26,7 +29,46 @@ void on_menu_open(GtkWindow *menuitem,
       char *filename;
       GtkFileChooser *chooser = GTK_FILE_CHOOSER(fileChooser);
       filename = gtk_file_chooser_get_filename(chooser);
-      sqlitemgt_is_database_open(filename);
+      sqlitemgt_open_database_with_path(filename);
+
+      g_free(filename);
+   }
+
+   gtk_widget_destroy(fileChooser);
+   /****************File Chooser***************/
+}
+
+void on_menu_save(GtkWindow *menuitem,
+                  gpointer user_data)
+{
+   g_message("save menu clicked!");
+   GtkWidget *fileChooser;
+   /****************File Chooser***************/
+   GtkFileFilter *filter;
+   GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_SAVE;
+   gint res;
+
+   fileChooser = gtk_file_chooser_dialog_new("Save File",
+                                             NULL,
+                                             action,
+                                             ("_Cancel"),
+                                             GTK_RESPONSE_CANCEL,
+                                             ("_Save"),
+                                             GTK_RESPONSE_ACCEPT,
+                                             NULL);
+
+   filter = gtk_file_filter_new(); // filter 1
+   gtk_file_filter_set_name(filter, "DataBase File");
+   gtk_file_filter_add_pattern(filter, ".db");
+   gtk_file_chooser_add_filter(GTK_FILE_CHOOSER(fileChooser), filter);
+
+   res = gtk_dialog_run(GTK_DIALOG(fileChooser));
+   if (res == GTK_RESPONSE_ACCEPT)
+   {
+      char *filename;
+      GtkFileChooser *chooser = GTK_FILE_CHOOSER(fileChooser);
+      filename = gtk_file_chooser_get_filename(chooser);
+      sqlitemgt_open_database_with_path(filename);
 
       g_free(filename);
    }
@@ -36,28 +78,50 @@ void on_menu_open(GtkWindow *menuitem,
 }
 
 /*********************create table*********************/
+
+void clicked_create_table_popupwindow(createtable, builder)
+GtkWidget *createtable;
+GtkBuilder *builder;
+{
+   GtkWidget *tablename, *columnsname;
+   const gchar *gtablename, *gcolumnsname;
+   tablename = GTK_WIDGET(gtk_builder_get_object(builder, "tablename"));
+   columnsname = GTK_WIDGET(gtk_builder_get_object(builder, "columnsname"));
+   checknullerror(tablename);
+   checknullerror(columnsname);
+   gtablename = gtk_entry_get_text(GTK_ENTRY(tablename));
+   gcolumnsname = gtk_entry_get_text(GTK_ENTRY(columnsname));
+   sqlitemgt_create_table(gtablename, gcolumnsname);
+   g_warning("CreateTable | %s %s.", gtablename, gcolumnsname);
+}
+
 void create_table_popupwindow(){
-    GtkWidget *window;
-    GtkBuilder *createtablebuilder;
 
-    createtablebuilder = gtk_builder_new();
-    gtk_builder_add_from_file(createtablebuilder, "xml/createtable.xml", NULL);
+   GtkWidget *window, *createtablebutton;
+   GtkBuilder *createtablebuilder;
 
-    window = GTK_WIDGET(gtk_builder_get_object(createtablebuilder, "createtablewindow"));
+   createtablebuilder = gtk_builder_new();
+   gtk_builder_add_from_file(createtablebuilder, "xml/createtable.xml", NULL);
 
-    gtk_widget_show_all(window);
+   window = GTK_WIDGET(gtk_builder_get_object(createtablebuilder, "createtablewindow"));
+   createtablebutton = GTK_WIDGET(gtk_builder_get_object(createtablebuilder, "createtablebutton"));
+
+   g_signal_connect(G_OBJECT(createtablebutton), "clicked", G_CALLBACK(clicked_create_table_popupwindow), createtablebuilder);
+
+   gtk_widget_show_all(window);
 }
 /*********************create table*********************/
 
 /*********************delete table*********************/
 void delete_table_popupwindow(){
-    GtkWidget *window;
-    GtkBuilder *createtablebuilder;
 
-    createtablebuilder = gtk_builder_new();
-    gtk_builder_add_from_file(createtablebuilder, "xml/deletetable.xml", NULL);
+   GtkWidget *window;
+   GtkBuilder *builder;
 
-    window = GTK_WIDGET(gtk_builder_get_object(createtablebuilder, "deletetablewindow"));
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file(builder, "xml/deletetable.xml", NULL);
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "deletetablewindow"));
 
     gtk_widget_show_all(window);
 }
@@ -65,13 +129,14 @@ void delete_table_popupwindow(){
 
 /*********************insert value*********************/
 void insert_value_popupwindow(){
-    GtkWidget *window;
-    GtkBuilder *createtablebuilder;
 
-    createtablebuilder = gtk_builder_new();
-    gtk_builder_add_from_file(createtablebuilder, "xml/insertvalue.xml", NULL);
+   GtkWidget *window;
+   GtkBuilder *builder;
 
-    window = GTK_WIDGET(gtk_builder_get_object(createtablebuilder, "insertvaluewindow"));
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file(builder, "xml/insertvalue.xml", NULL);
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "insertvaluewindow"));
 
     gtk_widget_show_all(window);
 }
@@ -79,13 +144,14 @@ void insert_value_popupwindow(){
 
 /*********************delete value*********************/
 void delete_value_popupwindow(){
-    GtkWidget *window;
-    GtkBuilder *createtablebuilder;
 
-    createtablebuilder = gtk_builder_new();
-    gtk_builder_add_from_file(createtablebuilder, "xml/deletevalue.xml", NULL);
+   GtkWidget *window;
+   GtkBuilder *builder;
 
-    window = GTK_WIDGET(gtk_builder_get_object(createtablebuilder, "deletevaluewindow"));
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file(builder, "xml/deletevalue.xml", NULL);
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "deletevaluewindow"));
 
     gtk_widget_show_all(window);
 }
--- library/gui/pages/popupwindow/popupwindows.h
+++ library/gui/pages/popupwindow/popupwindows.h
@@ -2,7 +2,10 @@
 #define SQLITEMGT_GUI_POPUPWINDOWS
 
 #include <gtk/gtk.h>
+#include <glib/gstdio.h>
+#include"../../../sqlite3mgt/sqlite3mgt.h"
 
+void on_menu_save(GtkWindow *menuitem, gpointer user_data);
 void edit_menu_create_table();
 void edit_menu_delete_table();
 void edit_menu_insert_value();
