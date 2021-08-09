f6ba485d96f7d313cebe31a8c98e851fa26e601f
omid reza keshtkar <omidnw@outlook.com>
2021-08-09 19:42:50 +0430

    update home file

--- library/gui/pages/home/home.c
+++ library/gui/pages/home/home.c
@@ -1,22 +1,28 @@
 #include "home.h"
-#include "../global/global.h"
 
 void create_home_page()
 {
-    GtkWidget *window, *homepagebox;
+    GtkWidget *window, *createtablefilemenu, *deletetablefilemenu, *insertvaluefilemenu, *deletevaluefilemenu, *quitmenu;
     GtkBuilder *homebuilder;
 
     homebuilder = gtk_builder_new();
     gtk_builder_add_from_file(homebuilder, "xml/homepage.xml", NULL);
 
     window = GTK_WIDGET(gtk_builder_get_object(homebuilder, "gtkhomepagewindow"));
-    homepagebox = GTK_WIDGET(gtk_builder_get_object(homebuilder, "welcometosqlitemanagementtext"));
+    createtablefilemenu = GTK_WIDGET(gtk_builder_get_object(homebuilder, "createtablefilemenu"));
+    deletetablefilemenu = GTK_WIDGET(gtk_builder_get_object(homebuilder, "deletetablefilemenu"));
+    insertvaluefilemenu = GTK_WIDGET(gtk_builder_get_object(homebuilder, "insertvaluefilemenu"));
+    deletevaluefilemenu = GTK_WIDGET(gtk_builder_get_object(homebuilder, "deletevaluefilemenu"));
+    quitmenu = GTK_WIDGET(gtk_builder_get_object(homebuilder, "quit"));
 
     /*********************check null*********************/
     checknullerror(window);
-    checknullerror(homepagebox);
+    checknullerror(createtablefilemenu);
+    checknullerror(deletetablefilemenu);
+    checknullerror(insertvaluefilemenu);
+    checknullerror(deletevaluefilemenu);
     /*********************check null*********************/
-
+    g_message("create home page successfuly!");
     GtkCssProvider *cssProvider = gtk_css_provider_new();
     if(cssProvider == NULL) g_error("Bye Css"); 
     gtk_css_provider_load_from_path(cssProvider, "xml/css/homepage.css", NULL);
@@ -25,6 +31,12 @@ void create_home_page()
                                GTK_STYLE_PROVIDER_PRIORITY_USER);
 
     sqlitemgt_set_global_gui_settings(window, "home");
+
+    g_signal_connect(G_OBJECT(quitmenu), "activate", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(G_OBJECT(createtablefilemenu), "activate", G_CALLBACK(edit_menu_create_table), NULL);
+    g_signal_connect(G_OBJECT(deletetablefilemenu), "activate", G_CALLBACK(edit_menu_delete_table), NULL);
+    g_signal_connect(G_OBJECT(insertvaluefilemenu), "activate", G_CALLBACK(edit_menu_insert_value), NULL);
+    g_signal_connect(G_OBJECT(deletevaluefilemenu), "activate", G_CALLBACK(edit_menu_delete_value), NULL);
 
     gtk_widget_show(window);
 }
--- library/gui/pages/home/home.h
+++ library/gui/pages/home/home.h
@@ -2,6 +2,9 @@
 #define SQLITE3MGT_GUI_HOME_PAGE_H
 
 #include <gtk/gtk.h>
+#include "../global/global.h"
+#include "../popupwindow/popupwindows.h"
+#include "library/sqlite3mgt/sqlite3mgt.h"
 #include "library/gui/pages/getosinfo/getosinfo.h"
 
 void create_home_page();
