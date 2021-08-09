2173a268e26c7617dca2d03937f6daf0ce16b51a
omid reza keshtkar <omidnw@outlook.com>
2021-08-09 23:24:19 +0430

    update popupwindow file

--- library/gui/pages/popupwindow/popupwindows.c
+++ library/gui/pages/popupwindow/popupwindows.c
@@ -92,66 +92,129 @@ GtkBuilder *builder;
    gtablename = gtk_entry_get_text(GTK_ENTRY(tablename));
    gcolumnsname = gtk_entry_get_text(GTK_ENTRY(columnsname));
    sqlitemgt_create_table(gtablename, gcolumnsname);
-   g_warning("CreateTable | %s %s.", gtablename, gcolumnsname);
+   g_warning("Create Table | %s %s.", gtablename, gcolumnsname);
 }
 
 void create_table_popupwindow(){
 
    GtkWidget *window, *createtablebutton;
-   GtkBuilder *createtablebuilder;
+   GtkBuilder *builder;
 
-   createtablebuilder = gtk_builder_new();
-   gtk_builder_add_from_file(createtablebuilder, "xml/createtable.xml", NULL);
+   builder = gtk_builder_new();
+   gtk_builder_add_from_file(builder, "xml/createtable.xml", NULL);
 
-   window = GTK_WIDGET(gtk_builder_get_object(createtablebuilder, "createtablewindow"));
-   createtablebutton = GTK_WIDGET(gtk_builder_get_object(createtablebuilder, "createtablebutton"));
+   window = GTK_WIDGET(gtk_builder_get_object(builder, "createtablewindow"));
+   createtablebutton = GTK_WIDGET(gtk_builder_get_object(builder, "createtablebutton"));
 
-   g_signal_connect(G_OBJECT(createtablebutton), "clicked", G_CALLBACK(clicked_create_table_popupwindow), createtablebuilder);
+   g_signal_connect(G_OBJECT(createtablebutton), "clicked", G_CALLBACK(clicked_create_table_popupwindow), builder);
 
    gtk_widget_show_all(window);
 }
 /*********************create table*********************/
 
 /*********************delete table*********************/
+
+void clicked_delete_table_popupwindow(deletetable, builder)
+GtkWidget *deletetable;
+GtkBuilder *builder;
+{
+   GtkWidget *tablename;
+   const gchar *gtablename;
+   tablename = GTK_WIDGET(gtk_builder_get_object(builder, "deletetablenamevalue"));
+   checknullerror(tablename);
+   gtablename = gtk_entry_get_text(GTK_ENTRY(tablename));
+   sqlitemgt_delete_table(gtablename);
+   g_warning("Delete Table | %s.", gtablename);
+}
+
 void delete_table_popupwindow(){
 
-   GtkWidget *window;
+   GtkWidget *window, *deletetablebutton;
    GtkBuilder *builder;
 
     builder = gtk_builder_new();
     gtk_builder_add_from_file(builder, "xml/deletetable.xml", NULL);
 
     window = GTK_WIDGET(gtk_builder_get_object(builder, "deletetablewindow"));
+    deletetablebutton = GTK_WIDGET(gtk_builder_get_object(builder, "deletetablebutton"));
+
+    g_signal_connect(G_OBJECT(deletetablebutton), "clicked", G_CALLBACK(clicked_delete_table_popupwindow), builder);
 
     gtk_widget_show_all(window);
 }
 /*********************delete table*********************/
 
 /*********************insert value*********************/
+
+void clicked_insert_value_popupwindow(insertvalue, builder)
+GtkWidget *insertvalue;
+GtkBuilder *builder;
+{
+   GtkWidget *tablename, *columnsname, *value;
+   const gchar *gtablename, *gcolumnsname, *gvalue;;
+   tablename = GTK_WIDGET(gtk_builder_get_object(builder, "tablename"));
+   columnsname = GTK_WIDGET(gtk_builder_get_object(builder, "columnsname"));
+   value = GTK_WIDGET(gtk_builder_get_object(builder, "value"));
+   checknullerror(tablename);
+   checknullerror(columnsname);
+   checknullerror(value);
+   gtablename = gtk_entry_get_text(GTK_ENTRY(tablename));
+   gcolumnsname = gtk_entry_get_text(GTK_ENTRY(columnsname));
+   gvalue = gtk_entry_get_text(GTK_ENTRY(value));
+   sqlitemgt_insert_value_to_table(gtablename, gcolumnsname, gvalue);
+   g_warning("Insert Value | %s.", gtablename);
+}
+
 void insert_value_popupwindow(){
 
-   GtkWidget *window;
+   GtkWidget *window, *insertvalue;
    GtkBuilder *builder;
 
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "xml/insertvalue.xml", NULL);
+   builder = gtk_builder_new();
+   gtk_builder_add_from_file(builder, "xml/insertvalue.xml", NULL);
 
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "insertvaluewindow"));
+   window = GTK_WIDGET(gtk_builder_get_object(builder, "insertvaluewindow"));
+   insertvalue = GTK_WIDGET(gtk_builder_get_object(builder, "insertvalue"));
+
+   g_signal_connect(G_OBJECT(insertvalue), "clicked", G_CALLBACK(clicked_insert_value_popupwindow), builder);
 
     gtk_widget_show_all(window);
 }
 /*********************insert value*********************/
 
 /*********************delete value*********************/
+
+void clicked_delete_value_popupwindow(deletevalue, builder)
+GtkWidget *deletevalue;
+GtkBuilder *builder;
+{
+   GtkWidget *tablename, *columnname, *value;
+   const gchar *gtablename, *gcolumnname, *gvalue;;
+   tablename = GTK_WIDGET(gtk_builder_get_object(builder, "tablename"));
+   columnname = GTK_WIDGET(gtk_builder_get_object(builder, "columnname"));
+   value = GTK_WIDGET(gtk_builder_get_object(builder, "value"));
+   checknullerror(tablename);
+   checknullerror(columnname);
+   checknullerror(value);
+   gtablename = gtk_entry_get_text(GTK_ENTRY(tablename));
+   gcolumnname = gtk_entry_get_text(GTK_ENTRY(columnname));
+   gvalue = gtk_entry_get_text(GTK_ENTRY(value));
+   sqlitemgt_delete_value_from_table(gtablename, gcolumnname, gvalue);
+   g_warning("Delete Value | %s.", gtablename);
+}
+
 void delete_value_popupwindow(){
 
-   GtkWidget *window;
+   GtkWidget *window, *deletevalue;
    GtkBuilder *builder;
 
     builder = gtk_builder_new();
     gtk_builder_add_from_file(builder, "xml/deletevalue.xml", NULL);
 
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "deletevaluewindow"));
+   window = GTK_WIDGET(gtk_builder_get_object(builder, "deletevaluewindow"));
+   deletevalue = GTK_WIDGET(gtk_builder_get_object(builder, "deletevalue"));
+
+   g_signal_connect(G_OBJECT(deletevalue), "clicked", G_CALLBACK(clicked_delete_value_popupwindow), builder);
 
     gtk_widget_show_all(window);
 }
